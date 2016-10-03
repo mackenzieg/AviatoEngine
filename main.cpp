03e8f18883201073756b3abd6e892c48254d017e
Mackenzie Goodwin <MackenzieJamesGoodwin@gmail.com>
2016-10-03 17:56:49 -0400

    Exploring Epsilon for float and double

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
+#include <cstdio>
+#include <cfloat>
+#include <cmath>
 #include "math/Vector3.h"
 
 int main() {
+
+    float x;
+
+    *((int*) &x) = 1;
+    *((int*) &x) = *((int*) &x) | 104 << 23;
+
+    printf("Float system epsilon %.12f\n", FLT_EPSILON);
+    printf("Float calculated epsilon %.12f\n", x); //TODO use this epsilon for comparision
+                                                    //TODO epsilon for comparison
+                                                    // TODO should be a multiple
+    double y;
+
+    *((int*) &y) = 1;
+    *((int*) &y) = *((int*) &y) | 104 << 23;
+
+    printf("Double epsilon %.12f\n", (1 + pow(2, -52)) - 1);
 
     Vector3<double> v(1, 1, 1);
 
