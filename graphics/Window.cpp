67c7041d1d26b215df84fdd84f535ecdd13ad365
Nicholas Laskus <time.continnum@gmail.com>
2016-10-03 15:57:25 -0400

    Init Window

--- graphics/Window.cpp
+++ graphics/Window.cpp
@@ -1,3 +1,15 @@
 #include "Window.h"
 
-//FUK ME
+Window::Window()
+        : windowTitle(""),
+          windowWidth(0),
+          windowHeight(0) {
+
+}
+
+Window::Window(const char *windowTitle, int16_t windowHeight, int16_t windowWidth)
+        : windowTitle(windowTitle),
+          windowHeight(windowHeight),
+          windowWidth(windowWidth) {
+
+}
--- graphics/Window.h
+++ graphics/Window.h
@@ -1,9 +1,17 @@
 #ifndef AVIATOENGINE_WINDOW_H
 #define AVIATOENGINE_WINDOW_H
 
+#include <cstdint>
 
 class Window {
+private:
+    const char* windowTitle;
+    int16_t windowHeight, windowWidth;
 
+public:
+    Window();
+
+    Window(const char *windowTitle, int16_t windowHeight, int16_t windowWidth);
 };
 
 
