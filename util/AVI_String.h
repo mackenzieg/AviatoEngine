9e3d6cd163b6ec65158875580e8af9c5578f4595
Nicholas Laskus <time.continnum@gmail.com>
2016-10-05 16:50:57 -0400

    Fixed Major Switch Bug that caused AVI_STRNIG to break by cross declaring a struct of ref8/ref16.

--- util/AVI_String.h
+++ util/AVI_String.h
@@ -5,12 +5,12 @@
 
 #define MAX_ALLOCATION 1024
 
-#define AVI_STRING_TYPE_8 0
-#define AVI_STRING_TYPE_16 1
+constexpr int AVI_STRING_TYPE_8 = 0;
+constexpr int AVI_STRING_TYPE_16 = 1;
 
-#define AVI_STRING_TYPE_MASK 0b00000111
+constexpr unsigned char AVI_STRING_TYPE_MASK = 0b00000111;
 
-typedef char *str;
+typedef char* str;
 
 /* Tell compiler not to reorder in memory */
 struct __attribute__ ((__packed__)) AVI_String8 {
@@ -32,7 +32,7 @@ struct __attribute__ ((__packed__)) AVI_String16 {
 /* Finds the structure that matches the String size */
 #define STR_STRUCT(T, string) ((struct AVI_String##T *)((string) - sizeof(struct AVI_String##T)))
 /* Global variable that stores current structure reference */
-//#define STR_STRUCT_VARIABLE(T, name, string) struct AVI_String##T *name = (struct AVI_String##T*) ((string) - (sizeof(struct AVI_String##T)))
+#define STR_STRUCT_VARIABLE(T, name, string) struct AVI_String##T *name = (struct AVI_String##T*) ((string) - (sizeof(struct AVI_String##T)))
 
 static inline int strLength(const str string) {
     unsigned char flags = (unsigned char) string[-1];
@@ -49,12 +49,12 @@ static inline int strLength(const str string) {
 static inline int avaliableSpace(const str string) {
     unsigned char flags = (unsigned char) string[-1];
     switch(flags & AVI_STRING_TYPE_MASK) {
-        case AVI_STRING_TYPE_8:
-            struct AVI_String8* ref8 = (AVI_String8 *) ((struct AVR_String8*) (string) - sizeof(struct AVI_String8));
-            return ref8->allocate - ref8->length;
-        case AVI_STRING_TYPE_16:
-            struct AVI_String16* ref16 = (AVI_String16 *) ((struct AVR_String16*) (string) - sizeof(struct AVI_String16));
-            return ref16->allocate - ref16->length;
+        case AVI_STRING_TYPE_8: {
+            STR_STRUCT_VARIABLE(8, ref8, string);
+            return ref8->allocate - ref8->length; }
+        case AVI_STRING_TYPE_16: {
+            STR_STRUCT_VARIABLE(16, ref16, string);
+            return ref16->allocate - ref16->length; }
         default:
             return -1;
     }
