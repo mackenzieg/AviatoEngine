76dd442e90b22221d982491297bbe94a8628132c
Mackenzie Goodwin <MackenzieJamesGoodwin@gmail.com>
2016-10-05 16:30:21 -0400

    Plz fix

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@ using namespace std;
 
 int main() {
     std::cin.sync_with_stdio(0);
-    AVI_String *s = new AVI_String((char *) "asd");
-
-    cout << s << endl;
-    cout << "Test" << endl;
 
     return 0;
 }
